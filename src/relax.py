4a8e969e98973aa7c0c643deb0897d48e1726a5f
Agustin Do Canto <trabajosdocanto@gmail.com>
2024-12-23 22:34:36 -0300

    [ADD]: relax compile --project <name-of-project> command

--- src/relax.py
+++ src/relax.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import shutil
 import click
 
@@ -11,8 +12,8 @@ RELAX_ASCII = """
 -----------------------"""
 
 RELAX_VERSION="v1.0.0 (Alpha)"
-
 TEMPLATES_PATH = "./templates"
+BUILD_FILE_NAME = "build.py"
 
 MINIMAL_TEMPLATE = r"""
 \documentclass[a4paper,12pt]{article}
@@ -41,6 +42,30 @@ MINIMAL_TEMPLATE = r"""
 
 \end{document}
 """
+
+BUILD_FILE_CONTENT = r"""
+import os
+import subprocess
+
+MAIN_FILE_NAME = "main.tex"
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+MAIN_FILE_PATH = os.path.join(SCRIPT_DIR, MAIN_FILE_NAME)
+
+def compile(main_file_path):
+	if os.path.exists(main_file_path):
+		subprocess.run([
+			"pdflatex",
+			"-output-directory", SCRIPT_DIR,
+			main_file_path
+		])
+	else:
+		print(f"Error: '{main_file_path}' does not exist.")
+
+if __name__ == '__main__':
+	compile(MAIN_FILE_PATH)
+"""
+
+### Auxiliar functions ###
 
 def get_template(template_name):
 	if os.path.exists(TEMPLATES_PATH):
@@ -77,6 +102,9 @@ def generate(ctx, project, template):
 	else:
 		mainfile_name = "main.tex"
 		mainfile_path = os.path.join(project, mainfile_name)
+	
+		buildfile_path = os.path.join(project, BUILD_FILE_NAME) 
+
 		project_images_path = os.path.join(project, "img")
 
 		os.mkdir(project)
@@ -93,11 +121,28 @@ def generate(ctx, project, template):
 
 			with open(mainfile_path, "w") as mainfile:
 				mainfile.write(project_template)
+
+			with open(buildfile_path, "w") as buildfile:
+				buildfile.write(BUILD_FILE_CONTENT)
+
 		except:
 			shutil.rmtree(project)
 			print("Error opening the template, try with another name")
-		
 
+
+""" Uses pdflatex to compile the project into a pdf file """
+@relax.command()
+@click.option('--project', required=True, help='Must be the name of a project', type=str)
+@click.pass_context
+def compile(ctx, project):
+	build_file_path = os.path.join(project, BUILD_FILE_NAME)
+	if not os.path.exists(project):
+		print(f"Error: The path '{project}' does not exists.")
+	elif not os.path.exists(build_file_path):
+		print(f"Error: The path to '{BUILD_FILE_NAME}' build file does not exist")
+	else:
+		print(build_file_path)
+		subprocess.run(["python", build_file_path])
 
 if __name__ == '__main__':
 	relax()
