7b6a006d22ae0a3871622c10f8f20aafd604fa5e
Agustin Do Canto <trabajosdocanto@gmail.com>
2025-01-19 19:16:54 -0300

    [ADD]: install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
 main(){
-	cd src
-	python -m venv venv
-	source venv/bin/activate
-	pip install -r requeriments.txt
-	# make_alias # UNCOMMENT this if you wanna create the relax alias in your system
+    # Navigate to the src directory
+    cd src
+    # Create a virtual environment
+    python -m venv venv
+    # Activate the virtual environment
+    source venv/bin/activate
+    # Install the required dependencies
+    pip install -r requeriments.txt
+    # Create the symlink for the relax command
+    make_symlink
 }
 
-make_alias(){
-	CONFIG_FILE="$HOME/.bashrc"
+make_symlink(){
+    # Define the project directory, script path, and symlink path
+    PROJECT_DIR="$HOME/Escritorio/Proyectos/Relax-Project/Relax/src"
+    SCRIPT_PATH="$PROJECT_DIR/relax.py"
+    BIN_PATH="$HOME/.local/bin/relax"
 
-	ALIAS_COMMAND="alias relax='python ./src/relax.py'"
+    # Remove the previous symlink if it exists
+    if [ -f "$BIN_PATH" ]; then
+        echo "Removing the previous symlink..."
+        rm "$BIN_PATH"
+    fi
 
-	if ! grep -Fxq "$ALIAS_COMMAND" "$CONFIG_FILE"; then
-	    echo "$ALIAS_COMMAND" >> "$CONFIG_FILE"
-	    source ~/.bashrc
-	    echo "Alias added to $CONFIG_FILE"
-	else
-	    echo "Error: the alias already exists $CONFIG_FILE"
-	fi
+    # Create a new symlink pointing to the latest version of relax.py
+    ln -s "$SCRIPT_PATH" "$BIN_PATH"
+    echo "Symlink created at $BIN_PATH pointing to $SCRIPT_PATH"
 }
 
+# Run the main function
 main
