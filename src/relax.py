b844326542309bd4c664970ad92456866a15cd56
Agustin Do Canto <trabajosdocanto@gmail.com>
2025-01-19 19:16:25 -0300

    [ADD]: Definitions and relax.py

--- src/relax.py
+++ src/relax.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 import shutil
 import click
+from jinja2 import Template
+
 
 RELAX_ASCII = """
 -----------------------
@@ -12,58 +14,7 @@ RELAX_ASCII = """
 -----------------------"""
 
 RELAX_VERSION="v1.0.0 (Alpha)"
-TEMPLATES_PATH = "./templates"
-BUILD_FILE_NAME = "build.py"
 
-MINIMAL_TEMPLATE = r"""
-\documentclass[a4paper,12pt]{article}
-
-% Additional packages you may need
-\usepackage[utf8]{inputenc}   % UTF-8 character encoding
-\usepackage{amsmath}          % Package for advanced mathematics
-\usepackage{graphicx}         % Package to insert images
-\usepackage{hyperref}         % For links in the document
-\usepackage{geometry}         % For adjusting margins
-
-% Adjusting margins
-\geometry{top=2cm, bottom=2cm, left=2cm, right=2cm}
-
-% Document title
-\title{My LaTeX Document}
-\author{Document Author}
-\date{\today}  % Current date
-
-\begin{document}
-
-% Print the title
-\maketitle
-
-\section{Introduction}
-
-\end{document}
-"""
-
-BUILD_FILE_CONTENT = r"""
-import os
-import subprocess
-
-MAIN_FILE_NAME = "main.tex"
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-MAIN_FILE_PATH = os.path.join(SCRIPT_DIR, MAIN_FILE_NAME)
-
-def compile(main_file_path):
-	if os.path.exists(main_file_path):
-		subprocess.run([
-			"pdflatex",
-			"-output-directory", SCRIPT_DIR,
-			main_file_path
-		])
-	else:
-		print(f"Error: '{main_file_path}' does not exist.")
-
-if __name__ == '__main__':
-	compile(MAIN_FILE_PATH)
-"""
 
 ### Auxiliar functions ###
 
@@ -91,10 +42,59 @@ def hello():
 	print("\n Hello world! \n")
 
 
-""" Generte projects """
+def get_component_template(component, extension):
+	if extension == "tex":
+		template = r"""
+\documentclass[a4paper,12pt]{article}
+
+% Additional packages you may need
+\usepackage[utf8]{inputenc}   % UTF-8 character encoding
+\usepackage{amsmath}          % Package for advanced mathematics
+
+% Document title
+\title{My LaTeX Document}
+\author{Document Author}
+\date{\today}  % Current date
+
+\begin{document}
+
+% Print the title
+\maketitle
+
+\end{document}
+"""
+		return template
+
+	
+
+""" Generate components """
 @relax.command()
-@click.option('--project', required=True, help='With a given name creates a new project to work in.', type=str)
-@click.option('--template', help='Define the type of the template', type=str)
+@click.option('-c','--component', required=True, help='With a given name creates a new project to work in.', type=str)
+@click.pass_context
+def create(ctx, component):
+	# Declarates the file paths for creation
+	routes = [component, f"{component}/Img"]
+
+	component_name = os.path.basename(component.strip())
+	
+	file_extensions = ["tex"]
+
+	# Create the file paths
+	for route in routes:
+		os.mkdir(route)
+		os.makedirs(route, exist_ok=True)
+		print(f"CREATED: {route}")
+
+	for extension in file_extensions:
+		file_path = os.path.join(component, f"{component_name}.{extension}")
+		with open(file_path, 'w') as file:
+			file.write(get_component_template(component, extension))
+
+
+""" Generate projects """
+@relax.command()
+@click.option('-p','--project', required=True, help='With a given name creates a new project to work in.', type=str)
+@click.option('-t','--template', help='Define the type of the template', type=str)
 @click.pass_context
 def generate(ctx, project, template):
 	if os.path.exists(project):
@@ -103,38 +103,18 @@ def generate(ctx, project, template):
 		mainfile_name = "main.tex"
 		mainfile_path = os.path.join(project, mainfile_name)
 	
-		buildfile_path = os.path.join(project, BUILD_FILE_NAME) 
-
-		project_images_path = os.path.join(project, "img")
-
 		os.mkdir(project)
-		os.mkdir(project_images_path)
-
 		os.makedirs(project, exist_ok=True)
-		os.makedirs(project_images_path, exist_ok=True)
 
-		try:
-			if not template:
-				project_template = MINIMAL_TEMPLATE
-			else:
-				project_template = get_template(template)
+		create.callback(f"{project}/main")
 
-			with open(mainfile_path, "w") as mainfile:
-				mainfile.write(project_template)
-
-			with open(buildfile_path, "w") as buildfile:
-				buildfile.write(BUILD_FILE_CONTENT)
-
-		except:
-			shutil.rmtree(project)
-			print("Error opening the template, try with another name")
 
 
 """ Uses pdflatex to compile the project into a pdf file """
 @relax.command()
-@click.option('--project', required=True, help='Must be the name of a project', type=str)
+@click.option('-p','--project', required=True, help='Must be the name of a project', type=str)
 @click.pass_context
-def compile(ctx, project):
+def build(ctx, project):
 	build_file_path = os.path.join(project, BUILD_FILE_NAME)
 	if not os.path.exists(project):
 		print(f"Error: The path '{project}' does not exists.")
@@ -143,6 +123,8 @@ def compile(ctx, project):
 	else:
 		print(build_file_path)
 		subprocess.run(["python", build_file_path])
+
+
 
 if __name__ == '__main__':
 	relax()
